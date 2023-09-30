d4783c3fe909c795832fbbb4df9f80480c2121bb
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-10-01 01:10:20 +0200

    implementation of random ship placement, adding state and value to the fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Board } from "./Board";
 
 function App() {
-  const initialBoard: number[][] = [];
+  const initialBoard: { value: number; state: number }[][] = [];
+
   for (let i = 0; i < 10; i++) {
     initialBoard[i] = [];
     for (let j = 0; j < 10; j++) {
-      initialBoard[i][j] = 0;
+      initialBoard[i][j] = { value: 0, state: 0 };
     }
   }
 
--- src/Board/index.tsx
+++ src/Board/index.tsx
@@ -2,122 +2,139 @@ import React, { useState } from "react";
 import { Circle, StyledCell, TBody, Table, TableHeader } from "./styled";
 import { colorSwitcher } from "./colorSwitcher";
 
+type Cell = {
+  value: number;
+  state: number;
+};
+
 type BoardProps = {
-  boardArray: number[][];
+  boardArray: Cell[][];
 };
 
 export const Board: React.FC<BoardProps> = ({ boardArray }) => {
 
-const randomBoard: number[][] = [];
-
-for (let i = 0; i < 10; i++) {
-  randomBoard[i] = [];
-  for (let j = 0; j < 10; j++) {
-    randomBoard[i][j] = 0; // Ustaw początkowo wszystkie komórki na 0
+  const randomBoard: { value: number; state: number }[][] = [];
+  for (let i = 0; i < 10; i++) {
+    randomBoard[i] = [];
+    for (let j = 0; j < 10; j++) {
+      randomBoard[i][j] = { value: 0, state: 0 };
+    }
   }
-}
 
-// Funkcja do ustawiania zestawów jednek wokół siebie
-function placeSet(count: number, value: number, board: number[][]) {
-  const directions = [
-    [-1, 0], [1, 0], [0, -1], [0, 1], // Góra, dół, lewo, prawo
-    [-1, -1], [-1, 1], [1, -1], [1, 1], // Przekątne
-  ];
-
-  while (count > 0) {
-    const randomRow = Math.floor(Math.random() * 10);
-    const randomCol = Math.floor(Math.random() * 10);
-
-    if (board[randomRow][randomCol] === 0) {
-      // Sprawdź, czy można umieścić zestaw jednek w tym miejscu
+  function placeFiveAdjacent(board: { value: number; state: number }[][]) {
+    while (true) {
+      const randomRow = Math.floor(Math.random() * 10);
+      const randomCol = Math.floor(Math.random() * 10);
+  
+      // Wybierz losowo poziomy lub pionowy kierunek
+      const horizontal = Math.random() < 0.5;
+  
       let canPlace = true;
-
-      for (const [dx, dy] of directions) {
-        const newRow = randomRow + dx;
-        const newCol = randomCol + dy;
-
+  
+      // Sprawdź, czy można umieścić pięć sąsiednich pól "1" w wybranym kierunku
+      for (let i = 0; i < 5; i++) {
+        const newRow = horizontal ? randomRow : randomRow + i;
+        const newCol = horizontal ? randomCol + i : randomCol;
+  
         if (
           newRow < 0 ||
           newRow >= 10 ||
           newCol < 0 ||
           newCol >= 10 ||
-          board[newRow][newCol] !== 0
+          board[newRow][newCol].value !== 0
         ) {
           canPlace = false;
           break;
         }
       }
-
+  
       if (canPlace) {
-        // Umieść zestaw jednek wokół
-        board[randomRow][randomCol] = value;
-        for (const [dx, dy] of directions) {
-          const newRow = randomRow + dx;
-          const newCol = randomCol + dy;
-          board[newRow][newCol] = 0;
+        // Umieść pięć sąsiednich pól "1" w wybranym kierunku
+        for (let i = 0; i < 5; i++) {
+          const newRow = horizontal ? randomRow : randomRow + i;
+          const newCol = horizontal ? randomCol + i : randomCol;
+          board[newRow][newCol].value = 1;
         }
-        count--; // Zmniejsz licznik zestawów do umieszczenia
+        break;
       }
     }
   }
-}
-function placeTwoAdjacent(board: number[][], value: number) {
-  const directions = [
-    [-1, 0],
-    [1, 0],
-    [0, -1],
-    [0, 1], // Góra, dół, lewo, prawo
-    [-1, -1],
-    [-1, 1],
-    [1, -1],
-    [1, 1], // Przekątne
-  ];
-
-  while (true) {
-    const randomRow = Math.floor(Math.random() * 10);
-    const randomCol = Math.floor(Math.random() * 9); // Ograniczenie, aby zmieścić dwa sąsiednie pola
-
-    if (board[randomRow][randomCol] === 0 && board[randomRow][randomCol + 1] === 0) {
-      // Sprawdź, czy można umieścić dwa sąsiednie pola z wartością "value" w tym miejscu
-      let canPlace = true;
-
-      for (const [dx, dy] of directions) {
-        for (let i = 0; i < 2; i++) {
-          const newRow = randomRow + dx;
-          const newCol = randomCol + i * dy;
-
-          if (
-            newRow < 0 ||
-            newRow >= 10 ||
-            newCol < 0 ||
-            newCol >= 10 ||
-            (board[newRow][newCol] !== 0 && board[newRow][newCol] !== value)
-          ) {
-            canPlace = false;
+  function placeFourSequential(board: { value: number; state: number }[][]) {
+    while (true) {
+      const randomRow = Math.floor(Math.random() * 10);
+      const randomCol = Math.floor(Math.random() * 10);
+      const horizontal = Math.random() < 0.5;
+  
+      if (horizontal) {
+        if (randomCol + 3 < 10) {
+          let canPlace = true;
+          for (let col = randomCol; col < randomCol + 4; col++) {
+            if (board[randomRow][col].value === 1) {
+              canPlace = false;
+              break;
+            }
+            if (
+              randomRow > 0 && board[randomRow - 1][col].value === 1 ||
+              randomRow < 9 && board[randomRow + 1][col].value === 1 ||
+              (col > 0 && board[randomRow][col - 1].value === 1) ||
+              (col < 9 && board[randomRow][col + 1].value === 1) ||
+              (randomRow > 0 && col > 0 && board[randomRow - 1][col - 1].value === 1) ||
+              (randomRow > 0 && col < 9 && board[randomRow - 1][col + 1].value === 1) ||
+              (randomRow < 9 && col > 0 && board[randomRow + 1][col - 1].value === 1) ||
+              (randomRow < 9 && col < 9 && board[randomRow + 1][col + 1].value === 1)
+            ) {
+              canPlace = false;
+              break;
+            }
+          }
+  
+          if (canPlace) {
+            for (let col = randomCol; col < randomCol + 4; col++) {
+              board[randomRow][col].value = 1;
+            }
+            break;
+          }
+        }
+      } else {
+        if (randomRow + 3 < 10) {
+          let canPlace = true;
+          for (let row = randomRow; row < randomRow + 4; row++) {
+            if (board[row][randomCol].value === 1) {
+              canPlace = false;
+              break;
+            }
+            if (
+              randomCol > 0 && board[row][randomCol - 1].value === 1 ||
+              randomCol < 9 && board[row][randomCol + 1].value === 1 ||
+              (row > 0 && board[row - 1][randomCol].value === 1) ||
+              (row < 9 && board[row + 1][randomCol].value === 1) ||
+              (row > 0 && randomCol > 0 && board[row - 1][randomCol - 1].value === 1) ||
+              (row > 0 && randomCol < 9 && board[row - 1][randomCol + 1].value === 1) ||
+              (row < 9 && randomCol > 0 && board[row + 1][randomCol - 1].value === 1) ||
+              (row < 9 && randomCol < 9 && board[row + 1][randomCol + 1].value === 1)
+            ) {
+              canPlace = false;
+              break;
+            }
+          }
+  
+          if (canPlace) {
+            for (let row = randomRow; row < randomRow + 4; row++) {
+              board[row][randomCol].value = 1;
+            }
             break;
           }
         }
       }
-
-      if (canPlace) {
-        // Umieść dwa sąsiednie pola z wartością "value" otoczone polami "0"
-        for (let i = 0; i < 2; i++) {
-          board[randomRow][randomCol + i] = value;
-        }
-        break; // Zakończ pętlę po umieszczeniu dwóch pól "value"
-      }
     }
   }
-}
-
-
-
-placeSet(2, 1, randomBoard);
-placeTwoAdjacent(randomBoard, 1);
-placeTwoAdjacent(randomBoard, 1);
- // Dwa zestawy po 2 jednki
-
-
+  
+  
+  placeFiveAdjacent(randomBoard);
+  placeFourSequential(randomBoard);
+  placeFourSequential(randomBoard);
+  placeFourSequential(randomBoard);
+  placeFourSequential(randomBoard);
   const [board, setBoard] = useState(randomBoard);
 
   const columnHeaders = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
@@ -125,7 +142,7 @@ placeTwoAdjacent(randomBoard, 1);
 
   const handleCellClick = (rowIndex: number, columnIndex: number) => {
     const updatedBoard = [...board];
-    updatedBoard[rowIndex][columnIndex] = 2; // Ustaw odpowiedni stan, np. 2 oznacza pudło
+    updatedBoard[rowIndex][columnIndex] = { ...updatedBoard[rowIndex][columnIndex], state: 2 }; // Ustaw odpowiedni stan, np. 2 oznacza pudło
     setBoard(updatedBoard);
   };
 
@@ -150,10 +167,10 @@ placeTwoAdjacent(randomBoard, 1);
               <StyledCell
                 key={columnIndex}
                 onClick={() => handleCellClick(rowIndex, columnIndex)}
-                color={colorSwitcher(cell)}
+                color={colorSwitcher(cell.value)}
                 data-coordinates={`(${columnHeaders[columnIndex]}, ${rowIndex+1})`} // Przekazanie współrzędnych
               >
-                {cell}
+                {cell.value}
               </StyledCell>
             ))}
           </tr>

a1b65f37d66d66650c4591cfb9f3e36f03ac512e
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-10-04 00:15:24 +0200

    handleCellClick- working

--- src/Board/index.tsx
+++ src/Board/index.tsx
@@ -30,6 +30,17 @@ export const Board: React.FC<BoardProps> = ({ boardArray, oponentBoard }) => {
   const rowHeaders = Array.from({ length: 11 }, (_, i) => i + 1);
 
   const handleCellClick = (rowIndex: number, columnIndex: number) => {
+    const directions = [
+      [0, 1], // Góra
+      [0, -1], // Dół
+      [-1, 0], // Lewo
+      [1, 0], // Prawo
+      [1, 1],
+      [-1, -1],
+      [-1, 1],
+      [1, -1],
+    ];
+  
     if (board[rowIndex][columnIndex].state !== 0) {
       return;
     }
@@ -74,31 +85,6 @@ export const Board: React.FC<BoardProps> = ({ boardArray, oponentBoard }) => {
       }
   
       // Dodatkowy warunek dla sąsiednich pól
-      const directions = [
-        [0, 1], // Góra
-        [0, -1], // Dół
-        [-1, 0], // Lewo
-        [1, 0], // Prawo
-        [1, 1],
-        [-1, -1],
-        [-1, 1],
-        [1, -1]
-      ];
-  
-      for (const [dx, dy] of directions) {
-        const newRow = rowIndex + dy;
-        const newColumn = columnIndex + dx;
-  
-        if (
-          newRow >= 0 &&
-          newRow < 10 &&
-          newColumn >= 0 &&
-          newColumn < 10 &&
-          updatedBoard[newRow][newColumn].value === 0
-        ) {
-          updatedBoard[newRow][newColumn].state = 2;
-        }
-      }
   
       setBoard(updatedBoard);
     } else {
@@ -109,9 +95,34 @@ export const Board: React.FC<BoardProps> = ({ boardArray, oponentBoard }) => {
   
       setBoard(updatedBoard);
     }
+  
+    // Przeiteruj całą tablicę i dla wszystkich komórek o stanie 3 zmień ich sąsiednie pola o wartości 0 na stan 2
+    for (let row = 0; row < 10; row++) {
+      for (let col = 0; col < 10; col++) {
+        const cell = updatedBoard[row][col];
+  
+        if (cell.state === 3) {
+          for (const [dx, dy] of directions) {
+            const newRow = row + dy;
+            const newColumn = col + dx;
+  
+            if (
+              newRow >= 0 &&
+              newRow < 10 &&
+              newColumn >= 0 &&
+              newColumn < 10 &&
+              updatedBoard[newRow][newColumn].value === 0
+            ) {
+              updatedBoard[newRow][newColumn].state = 2;
+            }
+          }
+        }
+      }
+    }
+  
+    setBoard(updatedBoard);
   };
   
-
   return (
     <Table>
       <thead>
